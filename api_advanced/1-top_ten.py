e8b1784d57e83bb953b9658e2d383b91ed38749c
benjaminketteytagoe-alu <b.kettey-ta@alustudent.com>
2025-10-26 10:52:23 -0700

    modified

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -9,19 +9,19 @@ def top_ten(subreddit):
     if not subreddit or not isinstance(subreddit, str):
         print(None)
         return
-    
+
     headers = {'User-Agent': 'MyAPI/0.0.1'}
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     params = {'limit': 10}
-    
+
     try:
         response = requests.get(url, headers=headers, params=params,
                                 allow_redirects=False)
-        
+
         if response.status_code == 200:
             data = response.json()
             posts = data.get('data').get('children')
-            
+
             for i in range(10):
                 print(posts[i].get('data').get('title'))
         else:
