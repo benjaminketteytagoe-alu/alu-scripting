f8021af280b0675c32032b8b8c767dcb45bf9385
benjaminketteytagoe-alu <b.kettey-ta@alustudent.com>
2025-10-26 11:01:52 -0700

    modified

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,24 +1,23 @@
 #!/usr/bin/python3
-"""Script that fetch 10 hot post for a given subreddit."""
+"""Script that fetches 10 hot posts for a given subreddit."""
 import requests
 
 
 def top_ten(subreddit):
-    """Print the titles of the first 10 hot posts for a given subreddit.
-    If not a valid subreddit, print None."""
-    headers = {'User-Agent': 'MyAPI/0.0.1'}
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    """Print the titles of the first 10 hot posts for a given subreddit."""
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
     params = {'limit': 10}
 
-    response = requests.get(url, headers=headers, params=params,
-                            allow_redirects=False)
+    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
 
     if response.status_code == 200:
-        data = response.json()
-        children = data.get('data', {}).get('children', [])
-
-        for post in children:
-            title = post.get('data', {}).get('title')
-            print(title)
+        data = response.json().get('data', {}).get('children', [])
+        if not data:
+            print(None)
+            return
+        for post in data:
+            print(post.get('data', {}).get('title'))
     else:
         print(None)
+
