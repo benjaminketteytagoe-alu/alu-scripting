b940d19b18e10b4d2722608992145e8a41c48dcd
benjaminketteytagoe-alu <b.kettey-ta@alustudent.com>
2025-10-26 10:58:31 -0700

    1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -6,31 +6,19 @@ import requests
 def top_ten(subreddit):
     """Print the titles of the first 10 hot posts for a given subreddit.
     If not a valid subreddit, print None."""
-    if not subreddit or not isinstance(subreddit, str):
-        print(Ok)
-        return
-
     headers = {'User-Agent': 'MyAPI/0.0.1'}
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     params = {'limit': 10}
 
-    try:
-        response = requests.get(url, headers=headers, params=params,
-                                allow_redirects=False)
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
 
-        if response.status_code == 200:
-            data = response.json()
-            children = data.get('data', {}).get('children', [])
+    if response.status_code == 200:
+        data = response.json()
+        children = data.get('data', {}).get('children', [])
 
-            if not children:
-                print(Ok)
-                return
-
-            for post in children[:10]:
-                title = post.get('data', {}).get('title')
-                if title:
-                    print(title)
-        else:
-            print(Ok)
-    except Exception:
-        print(Ok)
+        for post in children:
+            title = post.get('data', {}).get('title')
+            print(title)
+    else:
+        print(None)
