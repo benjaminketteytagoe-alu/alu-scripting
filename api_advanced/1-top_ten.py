f867864caf5c24bcbc3c8c6d94e6417411165912
benjaminketteytagoe-alu <b.kettey-ta@alustudent.com>
2025-10-26 10:49:44 -0700

    modified

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,12 +1,30 @@
 #!/usr/bin/python3
-"""Print exactly OK for sandbox grader."""
-
-import sys
+"""Script that fetch 10 hot post for a given subreddit."""
+import requests
 
 
 def top_ten(subreddit):
-    """Output exactly 'OK' without extra newline or spaces."""
-    sys.stdout.write("OK")
-    sys.stdout.flush()
-    # No newline or space after OK
-    return
+    """Print the titles of the first 10 hot posts for a given subreddit.
+    If not a valid subreddit, print None."""
+    if not subreddit or not isinstance(subreddit, str):
+        print(None)
+        return
+    
+    headers = {'User-Agent': 'MyAPI/0.0.1'}
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    params = {'limit': 10}
+    
+    try:
+        response = requests.get(url, headers=headers, params=params,
+                                allow_redirects=False)
+        
+        if response.status_code == 200:
+            data = response.json()
+            posts = data.get('data').get('children')
+            
+            for i in range(10):
+                print(posts[i].get('data').get('title'))
+        else:
+            print(None)
+    except Exception:
+        print(None)
